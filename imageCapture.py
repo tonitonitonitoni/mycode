5bedc5a80769a1a1b8d2d944b7d766af0338ea8b
tonitonitonitoni <77053314+tonitonitonitoni@users.noreply.github.com>
2024-09-27 15:32:50 -0400

    Add files via upload

---
+++ imageCapture.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 15 17:20:56 2024
+GOOD VIDEO CAPTURE FILE and no I didn't write this myself holy frig
+but I don't have a reference ror it sadly
+@author: unknown
+"""
+
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+
+cv2.destroyAllWindows()
